50f4419e9e3dba57a2f880df930a8c4786a74f49
Bruno Philipe <brunophilipe@me.com>
2017-08-23 20:30:12 +0200

    Initial Commit

---
+++ SwiftRPC/SwiftRPC.h
@@ -0,0 +1,19 @@
+//
+//  SwiftRPC.h
+//  SwiftRPC
+//
+//  Created by Bruno Philipe on 23/8/17.
+//  Copyright © 2017 Bruno Philipe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftRPC.
+FOUNDATION_EXPORT double SwiftRPCVersionNumber;
+
+//! Project version string for SwiftRPC.
+FOUNDATION_EXPORT const unsigned char SwiftRPCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftRPC/PublicHeader.h>
+
+
